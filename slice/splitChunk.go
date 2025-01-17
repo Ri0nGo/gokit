4d77781bd29189e03107b7e880e81840de4d14c9
Ri0nGo <920728039@qq.com>
2025-01-17 11:52:42 +0800

    splitChunk.go 新增函数说明

--- slice/splitChunk.go
+++ slice/splitChunk.go
@@ -4,6 +4,9 @@ import (
 	"github.com/Ri0nGo/gokit/utils"
 )
 
+// SplitChunk[T any] 将slice按照splitSize切割成多段
+// 例如：[1,2,3,4,5,6,7,8,9]；splitSize = 4
+// 结果：[[1,2,3,4], [5,6,7,8], [9]]
 func SplitChunk[T any](slice []T, splitSize int) ([][]T, error) {
 	if splitSize <= 0 || splitSize > len(slice) {
 		return nil, ErrSplitSizeOutOfSlice
@@ -24,6 +27,9 @@ func SplitChunk[T any](slice []T, splitSize int) ([][]T, error) {
 	return result, nil
 }
 
+// SplitChunkV2[T any] 将slice按照splitSize切割成多段
+// 例如：[1,2,3,4,5,6,7,8,9]；splitSize = 4
+// 结果：[[1,2,3,4], [5,6,7,8], [9]]
 func SplitChunkV2[T any](slice []T, splitSize int) ([][]T, error) {
 	if splitSize <= 0 || splitSize > len(slice) {
 		return nil, ErrSplitSizeOutOfSlice
